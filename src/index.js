80f11997daf056a8e639d4268978bb410111e04d
Lasse S. Haslev <lasse@haslev.no>
2017-04-29 12:53:37 +0200

    Added InputElement available through index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import BaseImageInput from './components/BaseImageInput';
 import ImageInput from './components/ImageInput';
 
+import BaseImageInputElement from './components/BaseImageInputElement';
+import ImageInputElement from './components/ImageInputElement';
+
 export default BaseImageInput;
 export {
     BaseImageInput,
     ImageInput,
+
+    BaseImageInputElement,
+    ImageInputElement,
 }
